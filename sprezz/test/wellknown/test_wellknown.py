4e48fc06019d5ce89b0a17db48cfe40f6cfcba51
Olaf Conradi <olaf@conradi.org>
2013-09-17 07:50:38 +0200

    First go at testing views
    Testing the zot-info view for parameters

--- sprezz/test/wellknown/test_wellknown.py
+++ sprezz/test/wellknown/test_wellknown.py
@@ -1,8 +1,15 @@
+import logging
 import unittest
+import sys
 
 from pyramid import testing
+from unittest.mock import patch
 
 from ..testing import create_single_content_registry
+
+
+log = logging.getLogger()
+log.level = logging.DEBUG
 
 
 class TestWellKnown(unittest.TestCase):
@@ -26,3 +33,43 @@ class TestWellKnown(unittest.TestCase):
         registry = create_single_content_registry(ob)
         inst.after_create(None, registry)
         self.assertEqual(Z['zot-info'], ob)
+
+
+class TestZotInfoProtocol(unittest.TestCase):
+    def setUp(self):
+        #stream_handler = logging.StreamHandler(sys.stdout)
+        #log.addHandler(stream_handler)
+        request = testing.DummyRequest()
+        self.config = testing.setUp(request=request)
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def _makeOne(self, context, request):
+        from sprezz.wellknown import ZotInfoProtocol
+        return ZotInfoProtocol(context, request)
+
+    def test_zot_info_no_key(self):
+        context = testing.DummyResource()
+        request = testing.DummyRequest(params={'target': 'target'})
+        request.graph = None
+        inst = self._makeOne(context, request)
+        result = inst.zot_info()
+        self.assertFalse(result['success'])
+        self.assertEquals(result['message'],
+                          'No key or target signature supplied.')
+
+    @patch('sprezz.wellknown.PersistentRSAKey.verify_message',
+           return_value=False)
+    def test_zot_info_invalid(self, rsa_mock):
+        context = testing.DummyResource()
+        request = testing.DummyRequest(params={'target': 'target',
+                                               'target_sig': 'sig',
+                                               'key': 'key'},
+                                       method='POST')
+        request.graph = None
+        inst = self._makeOne(context, request)
+        result = inst.zot_info()
+        self.assertFalse(result['success'])
+        self.assertEquals(result['message'],
+                          'Invalid target signature.')
